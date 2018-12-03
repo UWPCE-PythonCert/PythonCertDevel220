e447b61767fb58fd5afa5b68d2f34a34a8382d64
Andy Miles <akmiles@icloud.com>
2018-12-03 07:23:15 -0800

    fix rec depth

--- source/conf.py
+++ source/conf.py
@@ -31,6 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sys; sys.setrecursionlimit(1500)
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
