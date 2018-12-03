8116634e7989ed3298e06a7575512c524e1c2ddb
Andy Miles <akmiles@icloud.com>
2018-12-03 07:27:49 -0800

    bumped higher

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 import sys
-sys.setrecursionlimit(3000)
+sys.setrecursionlimit(10000)
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
